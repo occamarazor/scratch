d4c9493c00e9d47217e989ae76d1a54d7521a7b8
Occama Razor <30173929+occamarazor@users.noreply.github.com>
2025-10-20 17:50:44 +0300

    App: main.ts types

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
-const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule);
+const bootstrap = async (): Promise<void> => {
+  const app: INestApplication = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 };
 
